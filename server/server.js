b7a129a92731d5eeec3a0f9268c61a63aef8362d
VedanthVDev <vedanthvdev1996@gmail.com>
2023-02-24 11:08:56 +0000

    Fix cors

--- server/server.js
+++ server/server.js
@@ -7,6 +7,16 @@ const PORT = 3000;
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+  next();
+});
+
 app.use(express.json());
 app.use(cors());
 
