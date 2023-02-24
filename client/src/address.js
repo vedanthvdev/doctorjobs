be0d238ae088cd65d6f1aeda3f015649a230f900
VedanthVDev <vedanthvdev1996@gmail.com>
2023-02-24 12:55:53 +0000

    icon update

--- client/src/address.js
+++ client/src/address.js
@@ -1,2 +1,2 @@
-export const ipAddress = "https://hospital-job-server.onrender.com";
-// export const ipAddress = "http://localhost:3000";
+// export const ipAddress = "https://hospital-job-server.onrender.com";
+export const ipAddress = "http://localhost:3000";
