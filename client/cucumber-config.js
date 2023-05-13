0cdac68ba5be153c8f05a8f2dc8d776d6d5b3b19
VedanthVDev <vedanthvdev1996@gmail.com>
2023-05-13 23:05:27 +0100

    color change, audit, parallel, multidevice support

--- client/cucumber-config.js
+++ client/cucumber-config.js
@@ -1,9 +1,16 @@
+const cucumber_theme =
+  "--format @cucumber/pretty-formatter " +
+  "--format-options '{\"colorsEnabled\": true}' " +
+  '--format-options \'{"theme": {"feature keyword":["bold","blue"],"tag":["yellow"],"feature name":["blue","underline"],"feature description":["blueBright"],"scenario keyword":["bold","magenta"],"scenario name":["magenta","underline"],"step keyword":["bold","green"],"step text":["greenBright","italic"]}}\'';
+
 let cucumberOptions = [
-  "-f @cucumber/pretty-formatter",
+  cucumber_theme,
   "--require ./seleniumTests/steps/**/*.js",
   "--tags '@run_all'",
   "./seleniumTests/features/**/*.feature",
   "--require cucumber-parallel",
+  "--publish-quiet",
+  "--parallel 3",
 ].join(" ");
 
 module.exports = { default: cucumberOptions };
